554898e081f3b075c09de6672dc62f76da1591ba
Nicolas Pope <nwpope@gmail.com>
2014-10-10 16:03:58 +0200

    Basic layout template 1

--- js/web/dsbui.js
+++ js/web/dsbui.js
@@ -3,6 +3,28 @@
 		this.dsb = dsb;
 	};
 
+	DSBUI.prototype.makeToolbar = function() {
+		$('#myToolbar').w2toolbar({
+		    name : 'myToolbar',
+		    items: [
+		        { type: 'check',  id: 'item1', caption: 'Check', img: 'icon-add', checked: true },
+		        { type: 'break' },
+		        { type: 'menu',   id: 'item2', caption: 'Fabric', img: 'icon-folder', 
+		            items: [
+		                { text: 'Connect', img: 'icon-page' }, 
+		                { text: 'Disconnect', img: 'icon-page' }, 
+		                { text: 'Item 3', img: 'icon-page' }
+		            ]
+		        },
+		        { type: 'break' },
+		        { type: 'radio',  id: 'item3',  group: '1', caption: 'Radio 1', img: 'icon-page' },
+		        { type: 'radio',  id: 'item4',  group: '1', caption: 'Radio 2', img: 'icon-page' },
+		        { type: 'spacer' },
+		        { type: 'button',  id: 'item5',  caption: 'Item 5', img: 'icon-save' }
+		    ]
+		});
+	};
+
 	DSBUI.prototype.updateFabrics = function() {
 		this.dsb.getFabrics(function (data) {
 				document.getElementById('casmoutput').innerHTML = JSON.stringify(data);
